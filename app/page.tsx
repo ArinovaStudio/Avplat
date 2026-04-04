9fca5ce266d87948607f03f2890b30c1c8f1e21d
OM BHARGAV <ombhargav788@gmail.com>
2026-04-04 20:13:29 +0530

    optimized third section animation

--- app/page.tsx
+++ app/page.tsx
@@ -193,7 +193,7 @@ export default function Home() {
           {
             opacity: 0,
             ease: "power1.out",
-            duration: 0.4,
+            duration: 0.5,
           },
           "<"
         );
